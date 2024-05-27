23d3aa02f374de69afed8c125681a7ef02efd081
Kenneth De Winter <kenneth@dewinter.online>
2024-05-27 13:47:45 +0200

    Show lsp client in lualine
    Signed-off-by: Kenneth De Winter <kenneth@dewinter.online>

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -123,12 +123,27 @@ require("lazy").setup({
             "nvim-tree/nvim-web-devicons"
         },
         config = function()
+            local lsp_clients = function ()
+                local bufnr = vim.api.nvim_get_current_buf()
+                local clients = vim.lsp.buf_get_clients(bufnr)
+                if next(clients) == nil then
+                    return ""
+                end
+
+                local c = {}
+                for _, client in pairs(clients) do
+                    table.insert(c, client.name)
+                end
+                return "\u{f085}  " .. table.concat(c, " | ")
+            end
+
             require("lualine").setup({
                 options = {
                     --theme = "codedark"
                     theme = "vscode"
                 },
                 sections = {
+                    lualine_x = { lsp_clients, "encoding", "fileformat", "filetype" },
                     lualine_y = {} -- "progress"
                 }
             })
